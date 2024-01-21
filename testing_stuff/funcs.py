0ed615babaea402ad736d822f7b43b6b7e740b5d
Rohan Jha <rohan.jha@berkeley.edu>
2024-01-20 17:02:49 -0800

    clean-up

--- testing_stuff/funcs.py
+++ testing_stuff/funcs.py
@@ -6,110 +6,123 @@ import regex as re
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
+def main(string, df_name = 'df'):
+    if "ttest" in string:
+        ttest(string, df_name)
+    elif string.startswith("gen"):
+        filter_gen(string, df_name)
+    elif string.startswith('describe'):
+        describe(string, df_name)
+    elif string.startswith("corr"):
+        corr(string, df_name)
+    elif string.startswith("twoway"):
+        scatter(string, df_name)
+    elif string.startswith('histogram'):
+        hist(string, df_name)
+    elif string.startswith('reg'):
+        reg(string, df_name)
+    else:
+        raise ValueError(f'Your function is not supported')
+        
+
 def ttest(string, df_name = 'df'):
-    if "ttest" in string and "by(" in string:
-        eq_var = True
-        nan_pol = 'propagate'
-        
-        string = string.replace("ttest ", "")
-        catvar_split = string.split("(")
-        catvar = catvar_split[-1].split(")")[0]
-        if catvar_split[-1].split(")")[1].strip() == 'unequal':
-            eq_var = False
-        catvar_split[0] = "(".join(catvar_split[:-1])
-        var = catvar_split[0].split(",")[0]
-        if 'if !missing' in var:
-            var = var.split(" ")[0]
-            nan_pol = 'omit'
-        
-        print(f"catvar_vals = np.unique({df_name}['{catvar}'])")
-        print(f"if len(catvar_vals) != 2:")
-        print(f"    raise ValueError(f'The categorical variable ({catvar}) doesn\\'t have 2 groups')")
-        
-        print(f"df_1 = {df_name}[{df_name}['{catvar}'] == catvar_vals[0]]")
-        print(f"df_2 = {df_name}[{df_name}['{catvar}'] == catvar_vals[1]]")
-        
-        print(f"ttest = stats.ttest_ind(df_1['{var}'], df_2['{var}'], equal_var={eq_var}, nan_policy='{nan_pol}')")
-        
-        print(f"t_stat = ttest.statistic")
-        print(f"p_val = ttest.pvalue")
-        print("print(f'T-stat: {t_stat}, P-value: {p_val}')")
+    eq_var = True
+    nan_pol = 'propagate'
+
+    string = string.replace("ttest ", "")
+    catvar_split = string.split("(")
+    catvar = catvar_split[-1].split(")")[0]
+    if catvar_split[-1].split(")")[1].strip() == 'unequal':
+        eq_var = False
+    catvar_split[0] = "(".join(catvar_split[:-1])
+    var = catvar_split[0].split(",")[0]
+    if 'if !missing' in var:
+        var = var.split(" ")[0]
+        nan_pol = 'omit'
+
+    print(f"catvar_vals = np.unique({df_name}['{catvar}'])")
+    print(f"if len(catvar_vals) != 2:")
+    print(f"    raise ValueError(f'The categorical variable ({catvar}) doesn\\'t have 2 groups')")
+
+    print(f"df_1 = {df_name}[{df_name}['{catvar}'] == catvar_vals[0]]")
+    print(f"df_2 = {df_name}[{df_name}['{catvar}'] == catvar_vals[1]]")
+
+    print(f"ttest = stats.ttest_ind(df_1['{var}'], df_2['{var}'], equal_var={eq_var}, nan_policy='{nan_pol}')")
+
+    print(f"t_stat = ttest.statistic")
+    print(f"p_val = ttest.pvalue")
+    print("print(f'T-stat: {t_stat}, P-value: {p_val}')")
 
 def filter_gen(string, df_name='df'):
-    if string.startswith("gen"):
-        string = string.replace("gen ", "")
-        str_split = string.split("=",1)
-        str_split = [x.strip() for x in str_split]
-        new_col_name = str_split[0]
-        if str_split[1].count("=")>0:
-            if str_split[1].startswith("("):
-                str_split[1] = str_split[1][1:-1]
-            filter_split = str_split[1].split(" ", 1)
-            new_string = f"{df_name}['{new_col_name}'] = {df_name}['{filter_split[0]}'] {filter_split[1]}"
-            new_string_split = new_string.split(" = ", 1)
-            print(new_string)
-        else:
-            words_lst = re.findall(r'\w\w+',str_split[1])
-            for word in words_lst:
-                if word not in ['log']:
-                    str_split[1] = str_split[1].replace(word, f"{df_name}['{word}']")
-                else:
-                    str_split[1] = str_split[1].replace(word, f"np.{word}")
-            new_string = f"{df_name}['{new_col_name}'] = {str_split[1]}"
-            print(new_string)
+    string = string.replace("gen ", "")
+    str_split = string.split("=",1)
+    str_split = [x.strip() for x in str_split]
+    new_col_name = str_split[0]
+    if str_split[1].count("=")>0:
+        if str_split[1].startswith("("):
+            str_split[1] = str_split[1][1:-1]
+        filter_split = str_split[1].split(" ", 1)
+        new_string = f"{df_name}['{new_col_name}'] = {df_name}['{filter_split[0]}'] {filter_split[1]}"
+        new_string_split = new_string.split(" = ", 1)
+        print(new_string)
+    else:
+        words_lst = re.findall(r'\w\w+',str_split[1])
+        for word in words_lst:
+            if word not in ['log']:
+                str_split[1] = str_split[1].replace(word, f"{df_name}['{word}']")
+            else:
+                str_split[1] = str_split[1].replace(word, f"np.{word}")
+        new_string = f"{df_name}['{new_col_name}'] = {str_split[1]}"
+        print(new_string)
 
 def describe(string, df_name='df'):
-    if string.startswith('describe'):
-        print(f'{df_name}.describe()')
+    print(f'{df_name}.describe()')
             
 def corr(string, df_name='df'):
-    if string.startswith("corr"):
-        string = string.replace("corr ", "")
-        words_lst = re.findall(r'[a-zA-Z]+',string)
-        print(f"{df_name}[{words_lst}].corr()")
+    string = string.replace("corr ", "")
+    words_lst = re.findall(r'[a-zA-Z]+',string)
+    print(f"{df_name}[{words_lst}].corr()")
     
 def scatter(string, df_name = 'df'):
-    if string.startswith("twoway"):
-        string = string.replace("twoway (","")[:-2]
-        command = string.split(",")[0].strip()
-        customizations = string.split(",")[1].strip()
-        df = eval(df_name)
-        if command.startswith("scatter"):
-            var_1 = command.split(" ")[1]
-            var_2 = command.split(" ")[2]
-            print(f"plt.scatter({df_name}['{var_1}'], {df_name}['{var_2}']);")
-        customizations_split = customizations.split(")")
-        for word in customizations_split:
-            word = word.strip()
-            if word.startswith("xtitle"):
-                xtitle = word.split("(")[1]
-                if xtitle.startswith("'"):
-                    xtitle = xtitle[1:]
-                if xtitle.endswith("'"):
-                    xtitle = xtitle[:-1]
-                print(f"plt.xlabel('{xtitle}');")
-            if word.startswith("ytitle"):
-                ytitle = word.split("(")[1]
-                if ytitle.startswith("'"):
-                    ytitle = ytitle[1:]
-                if ytitle.endswith("'"):
-                    ytitle = ytitle[:-1]
-                print(f"plt.ylabel('{ytitle}');")
+    string = string.replace("twoway (","")[:-2]
+    command = string.split(",")[0].strip()
+    customizations = string.split(",")[1].strip()
+    df = eval(df_name)
+    if command.startswith("scatter"):
+        var_1 = command.split(" ")[1]
+        var_2 = command.split(" ")[2]
+        print(f"plt.scatter({df_name}['{var_1}'], {df_name}['{var_2}']);")
+    customizations_split = customizations.split(")")
+    for word in customizations_split:
+        word = word.strip()
+        if word.startswith("xtitle"):
+            xtitle = word.split("(")[1]
+            if xtitle.startswith("'"):
+                xtitle = xtitle[1:]
+            if xtitle.endswith("'"):
+                xtitle = xtitle[:-1]
+            print(f"plt.xlabel('{xtitle}');")
+        if word.startswith("ytitle"):
+            ytitle = word.split("(")[1]
+            if ytitle.startswith("'"):
+                ytitle = ytitle[1:]
+            if ytitle.endswith("'"):
+                ytitle = ytitle[:-1]
+            print(f"plt.ylabel('{ytitle}');")
 
 def hist(string, df_name='df'):
-    if string.startswith('histogram'):
-        string = string.replace('histogram ','')
-        str_split = string.split(", ")
-        num_bins_change = False
-        num_bins = 0
-        for word in str_split:
-            if word.startswith("bin"):
-                num_bins_change = True
-                num_bins = int(word.split("(")[1].split(")")[0])
-        if num_bins_change == False:
-            print(f"{df_name}.hist(column='{str_split[0]}');")
-        else:
-            print(f"{df_name}.hist(column='{str_split[0]}',bins={num_bins});")
+    string = string.replace('histogram ','')
+    str_split = string.split(", ")
+    num_bins_change = False
+    num_bins = 0
+    for word in str_split:
+        if word.startswith("bin"):
+            num_bins_change = True
+            num_bins = int(word.split("(")[1].split(")")[0])
+    if num_bins_change == False:
+        print(f"{df_name}.hist(column='{str_split[0]}');")
+    else:
+        print(f"{df_name}.hist(column='{str_split[0]}',bins={num_bins});")
             
 def reg(string, df_name = 'df'):
     string = string[4:]
@@ -194,7 +207,6 @@ def reg(string, df_name = 'df'):
     elif "vce" in second_half and "robust" in second_half:
         print("result = model.fit(cov_type='HC3')")
     elif "vce" in second_half and "cluster" in second_half:
-        #print(f"{df_name}{clustering_vars}") # {df_name}{clustering_vars}
         print(f"result = model.fit(cov_type='cluster', cov_kwds={{'groups': {df_name}{clustering_vars}}})")
     else:
         raise ValueError(f'VCE type not recognize')
