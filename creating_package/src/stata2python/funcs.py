0e80cfad55701bd0ce1ac3983c8b19e91d36cfc5
Rohan Jha <rohan.jha@berkeley.edu>
2024-02-06 15:29:55 -0800

    updates

--- creating_package/src/stata2python/funcs.py
+++ creating_package/src/stata2python/funcs.py
@@ -35,9 +35,11 @@ def ttest(string, df_name = 'df'):
     if 'if !missing' in var:
         var = var.split(" ")[0]
         nan_pol = 'omit'    
+    print("### First, we must filter the DataFrame to obtain the right values")
     print(f"catvar_vals = np.unique({df_name}['{catvar}'])")
     print(f"df_1 = {df_name}[{df_name}['{catvar}'] == catvar_vals[0]]")
     print(f"df_2 = {df_name}[{df_name}['{catvar}'] == catvar_vals[1]]")
+    print("### Then, we can run our t-test")
     print(f"stats.ttest_ind(df_1['{var}'], df_2['{var}'], equal_var={eq_var}, nan_policy='{nan_pol}')")
 
 def filter_gen(string, df_name='df'):
@@ -121,6 +123,7 @@ def reg(string, df_name = 'df'):
         first_half = string.split(",")[0]
         second_half = string.split(",")[1]
     if "if" in first_half:
+        print("### We first filter the DataFrame")
         if_statement = first_half.split("if")[1].strip()
         first_half = first_half.split("if")[0].strip()
         if "=" in if_statement:
@@ -138,6 +141,9 @@ def reg(string, df_name = 'df'):
     y_var = first_half.split(" ")[0]
     x_var = "'" + "', '".join(first_half.split(" ")[1:]) + "'"
     if "vce" in second_half and "cluster" in second_half:
+        print("### We first perform some string manipulations to ensure we are appropripately accounting for strings. ")
+        print("### This section seems complicated as this code is meant to be a general-purpose code for converting the Stata commands to Python.")
+        print("### You can greatly simplify this step if you know exactly which variables you are interested in; simply extract those variables from the DataFrame directly.")
         print(f"y_var = \"{y_var}\"")
         second_half_split = second_half.split("cluster")
         clustering_vars = second_half_split[1][:-1].strip().split(" ")
@@ -145,10 +151,13 @@ def reg(string, df_name = 'df'):
     if ' ' not in x_var:
         if clustering_vars:
             x_var_temp = x_var + ", '" + "', '".join(clustering_vars) + "'"
+            print("### Dropping NaN/missing values")
             print(f"{df_name}_no_na = {df_name}[[{x_var_temp},'{y_var}']].dropna()")
         else:
+            print("### Dropping NaN/missing values")
             print(f"{df_name}_no_na = {df_name}[[{x_var},'{y_var}']].dropna()")
         df_name = df_name + "_no_na"
+        print("### Below, we extract the relevant variables from the DataFrame")
         print(f"x_df = {df_name}[{x_var}]")
     elif "i." in x_var:
         #raise ValueError(f'Indicators are currently not supported')
@@ -157,6 +166,7 @@ def reg(string, df_name = 'df'):
         print(f"x_var = \"{x_var}\"")
         x_var_new = "'" + "', '".join([i.strip("'") for i in x_var.split(", ") if not i.strip("'").startswith("i.")]) + "'"
         print("x_var_new = \"'\" + \"', '\".join([i.strip(\"'\") for i in x_var.split(\", \") if not i.strip(\"'\").startswith(\"i.\")]) + \"'\"")
+        print("### The below section adds in relevant indicator variables, ensuring they have interpretable names")
         print(f"indicator_list = [m.strip(\"'\") for m in x_var.split('i.')[1:]]")
         print("for indicator in indicator_list:")
         print(f"    dummies = pd.get_dummies({df_name}[indicator])")
@@ -165,25 +175,33 @@ def reg(string, df_name = 'df'):
         print(f"    {df_name} = pd.concat([{df_name},dummies],axis=1)")
         print("    x_var_new = x_var_new + \", '\" + \"', '\".join(dummies.columns) + \"'\"")
         print("x_var = x_var_new")
+        print("### This helps ensure the clustered variables are extracted from the DataFrame")
         print("if clustering_vars:")
         print("    x_var_temp = x_var + \", '\" + \"', '\".join(clustering_vars) + \"'\"")
         if clustering_vars:
             x_var_temp = x_var + ", '" + "', '".join(clustering_vars) + "'"
             print(f"var_list = x_var_temp +\", '\"+ str(y_var) + \"'\"")
+            print("### Dropping NaN/missing values")
             print(f"{df_name}_no_na = {df_name}[[i.strip(\"'\") for i in var_list.split(\", \")]].dropna()")
         else:
+            print("### Dropping NaN/missing values")
             print(f"{df_name}_no_na = {df_name}[[{x_var},'{y_var}']].dropna()")
         df_name = df_name + "_no_na"
+        print("### Below, we extract the relevant variables from the DataFrame")
         print(f"x_df = {df_name}[[i.strip(\"'\") for i in x_var.split(\", \")]]")
     else:
         if clustering_vars:
             x_var_temp = x_var + ", '" + "', '".join(clustering_vars) + "'"
+            print("### Dropping NaN/missing values")
             print(f"{df_name}_no_na = {df_name}[[{x_var_temp},'{y_var}']].dropna()")
         else:
+            print("### Dropping NaN/missing values")
             print(f"{df_name}_no_na = {df_name}[[{x_var},'{y_var}']].dropna()")
         df_name = df_name + "_no_na"
+        print("### Below, we extract the relevant variables from the DataFrame")
         print(f"x_df = {df_name}[[{x_var}]]")
     print(f"y_df = {df_name}['{y_var}']")
+    print("### We now define the model, fit it to the data and then view a summary of the results")
     if 'noconstant' in second_half:
         print("model = sm.OLS(y_df, x_df)")
     else:
@@ -195,5 +213,5 @@ def reg(string, df_name = 'df'):
     elif "vce" in second_half and "cluster" in second_half:
         print(f"result = model.fit(cov_type='cluster', cov_kwds={{'groups': {df_name}{clustering_vars}}})")
     else:
-        raise ValueError(f'VCE type not recognize')
+        raise ValueError(f'VCE type not recognized')
     print("result.summary()")
